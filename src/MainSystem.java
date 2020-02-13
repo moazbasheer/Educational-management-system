54ccb7dcd78c1e7178be630e27732ac80d8f15f4
moaz basheer <moazbasheer97@gmail.com>
2020-02-13 23:41:24 +0200

    Update MainSystem.java

--- src/MainSystem.java
+++ src/MainSystem.java
@@ -1,10 +1,6 @@
-import java.util.*;
-
 public class MainSystem {
-	public static void main(String []args){
-		
-		MainControl pro=new MainControl();
+	public static void main(String[] args) {
+		MainControl pro = new MainControl();
 		pro.run();
-		
 	}
 }
